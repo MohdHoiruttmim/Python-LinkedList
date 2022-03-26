3a07dc4229b0874e0ca3023e114430951f9829a5
= <=>
2022-03-26 23:02:42 +0700

    pusing gess gabisa nambah data

--- linkedList.py
+++ linkedList.py
@@ -12,3 +12,24 @@ class Node:
     def setNext(self, newNext):
         self.next = newNext
 
+class linkedList:
+    def __init__(self):
+        self.head = None
+    def isEmpty(self):
+        return self.head == None
+   
+
+n1 = Node(21)
+print(n1) # node is stored at <__main__.Node object at 0x000001CC67A18B50>>
+# print(n1.getData()) # 21
+# print(n1.getNext()) # view next node
+
+n2 = Node(10)
+# print(n2)
+n2.setNext(n1)
+print(n2.getNext())
+
+myList = linkedList()
+print(myList.isEmpty())
+
+
