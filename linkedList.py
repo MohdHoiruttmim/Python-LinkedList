561aaea13243fec52d48ba14e64f1376d932bc23
= <=>
2022-03-27 09:36:22 +0700

    Know the size of the linked list

--- linkedList.py
+++ linkedList.py
@@ -17,7 +17,17 @@ class linkedList:
         self.head = None
     def isEmpty(self):
         return self.head == None
-   
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+    def size(self):
+        current = self.head
+        count = 0 
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
 
 n1 = Node(21)
 print(n1) # node is stored at <__main__.Node object at 0x000001CC67A18B50>>
@@ -30,6 +40,18 @@ n2.setNext(n1)
 print(n2.getNext())
 
 myList = linkedList()
+# print(myList.isEmpty())
+
+myList.add(99) # adding item to node
 print(myList.isEmpty())
+print(myList.head)
 
+myList.add(93)
+print(myList.head)
+print(myList.head.data)
 
+myList.add(97)
+print(myList.size())
+print(myList.head)
+# print(myList.head.getNext())
+print(myList.head.getNext().getNext())
